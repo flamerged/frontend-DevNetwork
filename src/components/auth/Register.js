2eb67ec042192ce2f297b4c9ec7d7b243dea46d1
flamerged <mersad.ajanovic@googlemail.com>
2020-08-18 00:57:08 +0200

    added user registration with axios for testing
    will be deleted to use Redux actions

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,12 +1,13 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
     const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        password: "",
-        password2: "",
+        name: "test",
+        email: "test@test.com",
+        password: "123456",
+        password2: "123456",
     });
 
     const { name, email, password, password2 } = formData;
@@ -14,12 +15,31 @@ const Register = () => {
     const onChange = (e) =>
         setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
         if (password !== password2) {
             console.log("Passwords do not match");
         } else {
-            console.log(formData);
+            const newUser = {
+                name,
+                email,
+                password,
+            };
+
+            try {
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                };
+
+                const body = JSON.stringify(newUser);
+
+                const res = await axios.post("api/users", body, config);
+                console.log(res.data);
+            } catch (error) {
+                console.error("there is a fucking error bitch");
+            }
         }
     };
 
